01c3057797c55688bf46247cf58a7ac0a709ce1f
Emilio Depena <emiliosdep@gmail.com>
2020-07-08 22:47:43 -0400

    post model

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const { check, validationResult } = require("express-validator");
-const router = express.Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const { check, validationResult } = require("express-validator");
+const router = express.Router();
 const isEmpty = require("../../utils/isEmpty");
 const config = require("../../config");
 
@@ -16,43 +16,43 @@ router.post(
   [
     check("email", "Email Required").not().isEmpty(),
     check("email", "Valid Email Required").isEmail(),
-    check("password", "Password must contain at least 6 characters").isLength({
-      min: 6,
-    }),
+    check(
+      "password",
+      "Please enter a password with at least 6 characters."
+    ).isLength({ min: 6 }),
   ],
   async (req, res) => {
     const errors = validationResult(req);
+
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
+    const { email, password } = req.body;
+    const userData = { email: email.toLowerCase(), password };
+
     try {
-      const userData = {
-        email: req.body.email,
-        password: req.body.password,
-      };
-
-      const existingUser = await User.findOne({ email: userData.email });
-      if (!isEmpty(existingUser)) {
-        return res.status(400).json({ errors: { email: "Email in use" } });
-      }
-
       const salt = await bcrypt.genSalt(10);
       userData.password = await bcrypt.hash(userData.password, salt);
 
+      // create the code that will check if the email is already in use before creating the new user.
+      const existing = await User.findOne({ email: userData.email });
+
+      if (!isEmpty(existing)) {
+        return res.status(400).json({ email: "email exists" });
+      }
+
       const user = await User.create(userData);
-      const keys = Object.keys(user._doc);
-      console.log(keys);
-      console.log(user);
-      return res.json(user);
+
+      res.json(user);
     } catch (error) {
       console.error(error);
-      return res.status(500).json(error);
+      res.status(500).json(error);
     }
   }
 );
 
-// @route		PUT api/users
-// @desc		login route
+// @route		PUT api/users/
+// @desc		login a user
 // @access	public
 router.put(
   "/",
@@ -68,33 +68,34 @@ router.put(
     }
 
     try {
-      const user = await User.findOne({ email: req.body.email });
+      const user = await User.findOne({ email: req.body.email.toLowerCase() });
 
       if (isEmpty(user)) {
-        return res.status(400).json({ errors: { message: "Invalid Login" } });
+        return req.status(404).json({ email: "email not found" });
       }
 
       const isMatch = await bcrypt.compare(req.body.password, user.password);
 
       if (!isMatch) {
-        return res.status(400).json({ errors: { message: "Invalid Login" } });
+        return req.status(401).json({ password: "incorrect password" });
       }
 
-      // information >> jwt encode + key >> token >> decode + key >> information
-      User.findByIdAndUpdate(user._id, { lastLogin: Date.now() });
+      // validated! challenge create the token and return it to the user.
 
-      //create the jwt token and return it to user. email and the id
+      User.findByIdAndUpdate(user.id, { lastLogin: Date.now() });
+
       const payload = {
-        id: user._id,
+        id: user.id,
         email: user.email,
+        //iat: Date
       };
 
       const token = jwt.sign(payload, config.secretOrKey, {});
 
-      res.json(token);
+      return res.json(token);
     } catch (error) {
       console.error(error);
-      return res.status(500).json(error);
+      res.status(500).json(error);
     }
   }
 );
